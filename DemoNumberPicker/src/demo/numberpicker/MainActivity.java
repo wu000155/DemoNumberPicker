78abf03d783f6d6b55d7f2607ea07228ad48ef63
Gerald Hurdle <hurdleg@algonquincollege.com>
2014-09-28 22:46:34 -0400

    Initial Commit. Default project with a single NumberPicker view.

---
+++ DemoNumberPicker/src/demo/numberpicker/MainActivity.java
@@ -0,0 +1,44 @@
+package demo.numberpicker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Demo of Android's Number Picker view.
+ *
+ * Tasks:
+ * 1) Perform a *thorough* code inspection.
+ *
+ * @author Gerald.Hurdle@AlgonquinCollege.com
+ * @version 1.0 Initial project with a single NumberPicker; no event handler
+ *
+ */
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
